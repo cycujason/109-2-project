c36ec5e1b371aa2b344541f770db8a1c76fd838b
kikoflame <kikoflame@gmail.com>
2021-08-12 23:48:35 +0800

    test python 0812

--- app.js
+++ app.js
@@ -4,13 +4,35 @@ const ejs = require('ejs');
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+let { PythonShell } = require('python-shell');
 var userRouter = require('./routes/users');
 var indexRouter = require('./routes/index');
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 
+app.get('/call/python', pythonProcess);
+
+function pythonProcess(req, res) {
+  let options = {
+    scriptPath: './',
+    args:
+      [
+        req.query.name,
+        req.query.from
+      ]
+  }
+  
+  PythonShell.run('process.py', options, (err, data) => {
+    if (err) res.send(err)
+    const parsedString = JSON.parse(data);
+    console.log(`name: ${parsedString.Name}, from: ${parsedString.From}`)
+    res.json(parsedString);
+  })
+  
+
+
+}//pythonprocess
 
 module.exports= app;
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Auth = require('../lib/auth');
+let { PythonShell } = require('python-shell');
+const { query } = require('express');
 
 
 router.get('/', (req, res) => {
@@ -17,4 +19,29 @@ router.get('/test1', (req, res) => {
 });
 
 */
+
+router.get('/call/python', pythonProcess);
+
+function pythonProcess(req, res) {
+  let options = {
+    args:
+      [
+        req.query.name,
+        req.query.from
+      ]
+  }
+  
+  PythonShell.run('./public/py/process.py', options, (err, data) => {
+    if (err) res.send(err)
+    const parsedString = JSON.parse(data);
+    console.log(`name: ${parsedString.Name}, from: ${parsedString.From}`)
+    res.json(parsedString);
+  })
+  
+
+
+}//pythonprocess
+
+
+
 module.exports = router;
