50364dd2b28e3f77881feb974c313a6ea7c8a412
kikoflame <kikoflame@gmail.com>
2021-08-26 23:51:37 +0800

    added and simple test of tag search

--- routes/users.js
+++ routes/users.js
@@ -42,6 +42,7 @@ router.get('/dashboard', Auth.checkNotAuthenticated, (req, res) => {
            keyword[i] = '%'+keyword[i]+'%';
          }//else to make the keyword searchable
       }//for to split users mutiple keyword
+      if(keyword.length!=1) keyword.push('%'+key+"%");
     }//if
     var range = req.query.range;
     var showSelect = true;
@@ -79,14 +80,18 @@ router.get('/dashboard', Auth.checkNotAuthenticated, (req, res) => {
       });//not consider the query fail
     }//else if
     else if(range === "tags"){  // need concat the search keyword(tag)
-      var all_condi = "";
+      var all_condi = "(";
+      var user_tags ="";
       for(num =0;num<keyword.length;num++){
-        var condi = "note_paragraph like "+keyword[i];
+        var condi = "(tags->>'key1' like '"+keyword[num]+"' or tags->>'key2' like '"+keyword[num]+"' or tags->>'key3' like '"+keyword[num]+"' or tags->>'key4' like '"+keyword[num]+"' or tags->>'key5' like '"+keyword[num]+"')"
         all_condi = all_condi+condi;
-        if(num+1!=keyword.length) all_condi = all_condi+" OR ";
+        user_tags = user_tags+" elem like '"+keyword[num]+"'"
+        if(num+1!=keyword.length) {all_condi = all_condi+" OR "; user_tags = user_tags + " OR "}//for
       }//for concat the query string
-      pool.query(`select * from note_content where create_user = $1 and (tags->>'key1' like $2 or tags->>'key2' like $2 or tags->>'key3' like $2 or tags->>'key4' like $2 or tags->>'key5' like $2)`,
-      [user,keyword],(err,results)=>{
+      all_condi = all_condi+") or";
+      all_condi = all_condi+"(    EXISTS (SELECT  FROM   unnest(user_tags) elem WHERE"+user_tags+"))"
+      pool.query(`select * from note_content where create_user = $1 and `+all_condi,
+      [user],(err,results)=>{
       res.render('dashboardT', { user: user, allnotes : results.rows ,limit:showSelect,nav:range, keyword:key});
       });//not consider the query fail
     }//else if
